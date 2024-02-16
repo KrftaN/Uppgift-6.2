4e47c32d5f473d0f217dc8365a168b7ff7f36f64
Admin <Admin@TEB12703>
2024-02-16 11:02:25 +0100

    Add project files.

---
+++ Uppgift 6.2/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Uppgift_6._1
+{
+    class Program
+    {
+        static int Störst(int tal1, int tal2)
+        {
+            if (tal1 > tal2)
+            {
+                return tal1;
+            }
+            else
+            {
+                return tal2;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv två tal separerad med ett kommatecken");
+            int[] numbers = Console.ReadLine().Split(',').Select(x => int.Parse(x.Trim())).ToArray();
+            int störst = Störst(numbers[0], numbers[1]);
+            Console.WriteLine($"{störst} är störst");
+
+        }
+    }
+}
